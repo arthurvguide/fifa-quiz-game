cc5d314219a4ae6848a848b8d1596555dd5b27bb
arthurvguide <arthurvguideuk@gmail.com>
2021-07-03 23:46:33 +0000

    Add java script questions arrays

---
+++ assets/js/question.js
@@ -0,0 +1,176 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questionsUefa = [  // question for UEFA league
+    {
+    numb: 1,
+    question: "Who has won the Champions League the most?",
+    answer: "Real Madrid",
+    options: [
+      "Barcelona",
+      "Manchester City",
+      "Liverpool",
+      "Real Madrid"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who has the most goals in Champions League history?",
+    answer: "Cristiano Ronaldo",
+    options: [
+      "Lewandowski",
+      "Cristiano Ronaldo",
+      "Messi",
+      "Sergio Aguero"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When was the first Champions League?",
+    answer: "1955",
+    options: [
+      "1980",
+      "1955",
+      "1975",
+      "1970"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the only French team to ever win the Champions League?",
+    answer: "Olympique Marseille",
+    options: [
+      "Olympique Marseille",
+      "Paris Saint-Germain F.C.",
+      "Olympique Lyonnais",
+      "AS Monaco"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is the country with the most Champions league titles",
+    answer: "Spain",
+    options: [
+      "England",
+      "Italy",
+      "Germany",
+      "Spain"
+    ]
+    },
+];
+  
+
+  let questionsPLeague = [  // question for Premier league
+    {
+    numb: 1,
+    question: "Who has won the most Premier League titles?",
+    answer: "Manchester United",
+    options: [
+      "Manchester United",
+      "Chelsea",
+      "Liverpool",
+      "Arsenal"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the top scorer in the premier league of all time",
+    answer: "Alan Shearer",
+    options: [
+      "Wayne Rooney",
+      "Cristiano Ronaldo",
+      "Alan Shearer",
+      "Sergio Aguero"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which player has provided the most Premier League assists?",
+    answer: "Ryan Giggs",
+    options: [
+      "Frank Lampard",
+      "Cesc Fàbregas",
+      "David Beckham",
+      "Ryan Giggs"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which goalkeeper has the best record in the Premier League?",
+    answer: "Petr Čech",
+    options: [
+      "David James",
+      "Petr Čech",
+      "Pepe Reina",
+      "Edwin Van der Sar"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which team has had the longest unbeaten run at home in the Premier League?",
+    answer: "Chelsea - 86 Games",
+    options: [
+      "Chelsea - 86 Games",
+      "Manchester United - 65 Games",
+      "Liverpool - 104 Games",
+      "West Ham - 62 Games"
+    ]
+  },
+];
+
+let questionsFifa = [  // question for Fifa World Cup
+    {
+    numb: 1,
+    question: "Which country have won the most World Cup tournaments trophies?",
+    answer: "Brazil",
+    options: [
+      "Brazil",
+      "England",
+      "Germany",
+      "France"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who Has scored the most goals in the World Cup?",
+    answer: "Miroslav Klose",
+    options: [
+      "Ronaldo Fenomeno",
+      "Maradona",
+      "Miroslav Klose",
+      "Pele"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the player that won more World Cups?",
+    answer: "Pele",
+    options: [
+      "Thomas Muller",
+      "Miroslav Klose",
+      "Ronaldinho Gaucho",
+      "Pele"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who was top scorer at the 2014 FIFA World Cup tournament?",
+    answer: "James Rodriguez",
+    options: [
+      "James Rodriguez",
+      "Thomas Müller",
+      "Toni Kroos",
+      "Messi"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which country won the first World Cup?",
+    answer: "Uruguay",
+    options: [
+      "Brazil",
+      "Hungary",
+      "Uruguay",
+      "Italy"
+    ]
+  },
+];
