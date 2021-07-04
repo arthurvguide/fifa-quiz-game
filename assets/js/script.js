135aabc529f1402250ab2dac221a4d6ca5ca9181
arthurvguide <arthurvguideuk@gmail.com>
2021-07-04 20:16:34 +0000

    If start button clicked JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
-// Getting Username Form - Beginning 
+//Selecting all required elements
+const startBtn = document.getElementsByClassName("username-btn")[0]; 
+const usernameScreen = document.getElementById("username-screen"); 
+const leagueScreen = document.getElementById("league-screen"); 
+const uefaBtn = document.getElementById("uefa");
+const premierBtn = document.getElementById("premier");
+const fifaBtn = document.getElementById("fifa");
+const questionsScreen = document.getElementById("questions-screen");
 
-function getFormDetails(event) {
+// if Start Button ( on Username Screen ) clicked
+
+function removeUsernameScreen(e){
+    usernameScreen.style.display = "none"; // hide Username Screen
+    leagueScreen.style.display = "flex";   // Show Select League Screen
+    return username = document.getElementById("username-input").value; // Get username value
+ 
+};
+
+startBtn.addEventListener("click", removeUsernameScreen); // Event to start button on click
+
+// If Select League buttons clicked 
+
+function uefaSelect(){ 
+    leagueScreen.style.display = "none"; // hide League Screen
+    questionsScreen.style.display = "flex"; // show league Screen
     
-    event.preventDefault();
-    
-    let username = document.getElementsByClassName("username-input");
-    console.log(username.value);
-}
+};
 
-let usernameForm = document.getElementsByClassName('username-form');
-usernameForm.addEventListener('submit', getFormDetails);
+uefaBtn.addEventListener("click", uefaSelect);
 
-// Getting Username Form - End
 
