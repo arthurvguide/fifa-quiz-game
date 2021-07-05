3294e942ebb19a137279906c732cda070ea1a69f
arthurvguide <arthurvguideuk@gmail.com>
2021-07-05 09:53:25 +0000

    Add Next Button Question Function

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,6 +1,6 @@
 // creating an array and passing the number, questions, options, and answers
 
-let questionsUefa = [  // question for UEFA league
+let questions = [  // question for UEFA league 1 to 5
     {
     numb: 1,
     question: "Who has won the Champions League the most?",
@@ -46,8 +46,8 @@ let questionsUefa = [  // question for UEFA league
     ]
   },
     {
-    numb: 5,
-    question: "Which is the country with the most Champions league titles",
+    numb: 5,  
+    question: "Which is the country with the most Champions league titles?",
     answer: "Spain",
     options: [
       "England",
@@ -56,70 +56,64 @@ let questionsUefa = [  // question for UEFA league
       "Spain"
     ]
     },
-];
-  
+    {
+      numb: 6,  // Questions for Premier League Beggining
+      question: "Who has won the most Premier League titles?",
+      answer: "Manchester United",
+      options: [
+        "Manchester United",
+        "Chelsea",
+        "Liverpool",
+        "Arsenal"
+      ]
+    },
+      {
+      numb: 7,
+      question: "Who is the top scorer in the premier league of all time",
+      answer: "Alan Shearer",
+      options: [
+        "Wayne Rooney",
+        "Cristiano Ronaldo",
+        "Alan Shearer",
+        "Sergio Aguero"
+      ]
+    },
+      {
+      numb: 8,
+      question: "Which player has provided the most Premier League assists?",
+      answer: "Ryan Giggs",
+      options: [
+        "Frank Lampard",
+        "Cesc Fàbregas",
+        "David Beckham",
+        "Ryan Giggs"
+      ]
+    },
+      {
+      numb: 9,
+      question: "Which goalkeeper has the best record in the Premier League?",
+      answer: "Petr Čech",
+      options: [
+        "David James",
+        "Petr Čech",
+        "Pepe Reina",
+        "Edwin Van der Sar"
+      ]
+    },
+      {
+      numb: 10,
+      question: "Which team has had the longest unbeaten run at home in the Premier League?",
+      answer: "Chelsea - 86 Games",
+      options: [
+        "Chelsea - 86 Games",
+        "Manchester United - 65 Games",
+        "Liverpool - 104 Games",
+        "West Ham - 62 Games"
+      ]
+    },
 
-  let questionsPLeague = [  // question for Premier league
     {
-    numb: 1,
-    question: "Who has won the most Premier League titles?",
-    answer: "Manchester United",
-    options: [
-      "Manchester United",
-      "Chelsea",
-      "Liverpool",
-      "Arsenal"
-    ]
-  },
-    {
-    numb: 2,
-    question: "Who is the top scorer in the premier league of all time",
-    answer: "Alan Shearer",
-    options: [
-      "Wayne Rooney",
-      "Cristiano Ronaldo",
-      "Alan Shearer",
-      "Sergio Aguero"
-    ]
-  },
-    {
-    numb: 3,
-    question: "Which player has provided the most Premier League assists?",
-    answer: "Ryan Giggs",
-    options: [
-      "Frank Lampard",
-      "Cesc Fàbregas",
-      "David Beckham",
-      "Ryan Giggs"
-    ]
-  },
-    {
-    numb: 4,
-    question: "Which goalkeeper has the best record in the Premier League?",
-    answer: "Petr Čech",
-    options: [
-      "David James",
-      "Petr Čech",
-      "Pepe Reina",
-      "Edwin Van der Sar"
-    ]
-  },
-    {
-    numb: 5,
-    question: "Which team has had the longest unbeaten run at home in the Premier League?",
-    answer: "Chelsea - 86 Games",
-    options: [
-      "Chelsea - 86 Games",
-      "Manchester United - 65 Games",
-      "Liverpool - 104 Games",
-      "West Ham - 62 Games"
-    ]
-  },
-];
-
-let questionsFifa = [  // question for Fifa World Cup
-    {
-    numb: 1,
+    numb: 11,   // Questions for World Cup Beggining
     question: "Which country have won the most World Cup tournaments trophies?",
     answer: "Brazil",
     options: [
@@ -130,7 +124,7 @@ let questionsFifa = [  // question for Fifa World Cup
     ]
   },
     {
-    numb: 2,
+    numb: 12,
     question: "Who Has scored the most goals in the World Cup?",
     answer: "Miroslav Klose",
     options: [
@@ -141,7 +135,7 @@ let questionsFifa = [  // question for Fifa World Cup
     ]
   },
     {
-    numb: 3,
+    numb: 13,
     question: "Who is the player that won more World Cups?",
     answer: "Pele",
     options: [
@@ -152,7 +146,7 @@ let questionsFifa = [  // question for Fifa World Cup
     ]
   },
     {
-    numb: 4,
+    numb: 14,
     question: "Who was top scorer at the 2014 FIFA World Cup tournament?",
     answer: "James Rodriguez",
     options: [
@@ -163,7 +157,7 @@ let questionsFifa = [  // question for Fifa World Cup
     ]
   },
     {
-    numb: 5,
+    numb: 15,
     question: "Which country won the first World Cup?",
     answer: "Uruguay",
     options: [
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Selecting all required elements
+//Selecting required elements
 const startBtn = document.getElementsByClassName("username-btn")[0]; 
 const usernameScreen = document.getElementById("username-screen"); 
 const leagueScreen = document.getElementById("league-screen"); 
@@ -6,6 +6,8 @@ const uefaBtn = document.getElementById("uefa");
 const premierBtn = document.getElementById("premier");
 const fifaBtn = document.getElementById("fifa");
 const questionsScreen = document.getElementById("questions-screen");
+const timer = document.getElementsByClassName("countdown")[0];
+const nextBtn = document.getElementsByClassName("next-btn")[0];
 
 // if Start Button ( on Username Screen ) clicked
 
@@ -25,42 +27,64 @@ startBtn.addEventListener("click", removeUsernameScreen); // Event to start butt
 
 // If Select League buttons clicked 
 
+let questCount = 0;  // counter for questions 
+let limitCount = 0; // This is for create a limit on ShowQuestions Functions, for only show up questions of each league
+
 function uefaSelect(){   // user wants to play uefa league
     leagueScreen.style.display = "none"; // hide League Screen
     questionsScreen.style.display = "flex"; // show league Screen
     showQuestions(0);
+    limitCount = 11;
 };
-
 uefaBtn.addEventListener("click", uefaSelect);  
 
 function premierSelect(){  // user wants to play premier league
     leagueScreen.style.display = "none"; // hide League Screen
     questionsScreen.style.display = "flex"; // show league Screen
-    showQuestions(0);
+    showQuestions(5);
+    questCount = 5;
+    limitCount = 6;
 }
 premierBtn.addEventListener("click", premierSelect);  
 
 function fifaSelect(){  // user wants to fifa league
     leagueScreen.style.display = "none"; // hide League Screen
     questionsScreen.style.display = "flex"; // show league Screen
-    showQuestions(0);
+    showQuestions(10);
+    questCount = 10;
+    limitCount = 1;
 }
 fifaBtn.addEventListener("click", fifaSelect);  
 
-// getting questions and options from array
+
+// If next button clicked on quiz game
+
+function nextQuestion() {     
+    if(questCount < questions.length - limitCount){
+        questCount++;
+        showQuestions(questCount);
+    }
+
+};
+
+nextBtn.addEventListener("click", nextQuestion);
+ 
+
+// getting questions and options from array and showing up
 
 function showQuestions(index) {
     const questionBox = document.getElementsByClassName("question-box")[0];
     const optionsBox = document.getElementsByClassName("options-box")[0];
-//creating a new span and div tag for question and option and passing the value using array index   
-    let que_tag = '<span><h4>'+ questionsUefa[index].numb + ". " + questionsUefa[index].question +'</h4></span>';
+
+    //creating a new span tag for question and option and passing the value using array index   
+    let queTag = '<span><h4>'+ questions[index].question +'</h4></span>';
     
-    let option_tag = 
-    '<span>'+ questionsUefa[index].options[0] +'</span>'
-    + '<span>'+ questionsUefa[index].options[1] +'</span>'
-    + '<span>'+ questionsUefa[index].options[2] +'</span>'
-    + '<span>'+ questionsUefa[index].options[3] +'</span>';
-    questionBox.innerHTML = que_tag; //adding new span tag inside que_tag
-    optionsBox.innerHTML = option_tag; //adding new spans tag inside option_tag
+    let optionTag = 
+    '<span>'+ questions[index].options[0] +'</span>'
+    + '<span>'+ questions[index].options[1] +'</span>'
+    + '<span>'+ questions[index].options[2] +'</span>'
+    + '<span>'+ questions[index].options[3] +'</span>';
+    questionBox.innerHTML = queTag; //adding new span tag inside que_tag
+    optionsBox.innerHTML = optionTag; //adding new spans tag inside option_tag
 };
    
